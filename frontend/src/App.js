33c6f78d2c5cd5d94b044b6f37e80e1a040e6c56
Ing. Yubisel Vega Alvrez <yubiselv@gmail.com>
2022-01-03 10:26:05 -0300

    fixing issue in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,23 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RouteConfig } from "router/Router";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import APP_CONFIG from 'config/app.config';
+
+const client = new ApolloClient({
+  uri: APP_CONFIG.GRAPHQL_URL,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <Router basename="/spacex-react">
-      <Header />
-      <Container fluid>
-        <RouteConfig base />
-      </Container>
+      <ApolloProvider client={client}>
+        <Header />
+        <Container fluid>
+          <RouteConfig base />
+        </Container>
+      </ApolloProvider>
     </Router>
   );
 }
